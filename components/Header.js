6334f7a279e6b3035ff2cbd47ffd9c736b3a6098
Billy Clinton Mabunda <mclintonsa@gmail.com>
2024-06-01 20:10:25 +0200

    Setting up project structure and added CSS styling

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img src="./images/troll-face.png" />
+            <h2>Header component</h2>
+            <h4>React Course - Project 3</h4>
+        </header>
+    )
+}
