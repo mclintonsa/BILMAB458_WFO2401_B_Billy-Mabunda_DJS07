1a18fe28b576d8cef43c49fe5b3cdd6a8110f038
Billy Clinton Mabunda <mclintonsa@gmail.com>
2024-06-03 01:54:03 +0200

    Added functionality to fetch meme images from API

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React from "react"
-import Header from "./components/Header.js"
-import Meme from "./components/Meme.js"
+import Header from "./components/Header"
+import Meme from "./components/Meme"
 /**
  * Challenge: 
  * - Create a Meme component.
